59c0465a9ed8b3d239380f760f816b538731a144
Kryštof Kosina <k.kosina@outlook.com>
2020-04-26 18:13:08 +0200

    Added oh-my-zsh

--- zsh/settings.zsh
+++ zsh/settings.zsh
@@ -1,32 +1,13 @@
-# Initialize completion
-autoload -Uz compinit && compinit -i
-zstyle ':completion:*' menu select=4
-zmodload zsh/complist
+PATH="$HOME/.local/bin${PATH:+:${PATH}}"
+EDITOR="nvim"
+ZSH=~/.oh-my-zsh/
+export TERM="st-256color"
+
 # Use vim style navigation keys in menu completion
 bindkey -M menuselect 'h' vi-backward-char
 bindkey -M menuselect 'k' vi-up-line-or-history
 bindkey -M menuselect 'l' vi-forward-char
 bindkey -M menuselect 'j' vi-down-line-or-history
-
-# Initialize editing command line
-autoload -U edit-command-line && zle -N edit-command-line
-
-# Enable interactive comments (# on the command line)
-setopt interactivecomments
-
-# Nicer history
-HISTSIZE=1048576
-HISTFILE="$HOME/.zsh_history"
-SAVEHIST=$HISTSIZE
-setopt appendhistory
-setopt incappendhistory
-setopt extendedhistory
-
-# Time to wait for additional characters in a sequence
-KEYTIMEOUT=1 # corresponds to 10ms
-
-# Use vim as the editor
-export EDITOR=nvim
 
 # Use vim style line editing in zsh
 bindkey -v

37daa071bdc626e2caed63a29413deefa0fce557
Kryštof Kosina <k.kosina@outlook.com>
2020-04-30 19:06:35 +0200

    ZSH plugins

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -9,6 +9,6 @@ ZSH_THEME="spaceship"
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git)
+plugins=(git zsh-autosuggestions zsh-syntax-highlighting)
 
 source $ZSH/oh-my-zsh.sh

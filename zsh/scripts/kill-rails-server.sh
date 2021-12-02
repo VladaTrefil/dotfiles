adc23202749974819c95de0e6908495f466b183e
Vlada <vladislav.trefil@protonmail.com>
2021-12-02 22:30:41 +0100

    scripts - tweak kill-rails-server

--- zsh/scripts/kill-rails-server.sh
+++ zsh/scripts/kill-rails-server.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-TASK=`lsof -wni tcp:3000 | grep -o -E "ruby\s*\d{3,5}" | head -1 | grep -E -o "\d{3,5}"`
+port=3000
+
+if [[ -n $1 ]]; then
+  port=$1
+fi
+
+TASK=`lsof -wni tcp:"$port" | grep -o -E "ruby\s*\d{3,5}" | head -1 | grep -E -o "\d{3,5}"`
 
 kill -9 $TASK

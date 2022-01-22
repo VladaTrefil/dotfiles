aacf4867e75895bd8c67a294f3c9ddeabfc6e79e
Vlada <vladislav.trefil@protonmail.com>
2022-01-22 20:25:15 +0100

    zsh - comment cleanup

--- zsh/settings.zsh
+++ zsh/settings.zsh
@@ -25,11 +25,7 @@ export LESS_TERMCAP_so=$'\e[01;33m'
 export LESS_TERMCAP_ue=$'\e[0m'
 export LESS_TERMCAP_us=$'\e[1;4;31m'
 
-# Which plugins would you like to load?
 # Standard plugins can be found in ~/.oh-my-zsh/plugins/*
-# Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
-# Example format: plugins=(rails git textmate ruby lighthouse)
-# Add wisely, as too many plugins slow down shell startup.
 plugins=(
   git 
   vi-mode 

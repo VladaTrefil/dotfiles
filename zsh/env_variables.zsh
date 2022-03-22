ea4a18b1a2c2023eb0027596640f2888d269cbdf
Vlada <vladislav.trefil@protonmail.com>
2022-03-22 01:20:33 +0100

    zsh - tweak env

--- zsh/env_variables.zsh
+++ zsh/env_variables.zsh
@@ -1,17 +1,25 @@
 export PKG_CONFIG_PATH=/usr/local/Cellar/imagemagick@6/6.9.12-6/lib/pkgconfig
 
+# ======= RubyOnRails =============================
+
 # Rails test fix
 export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
 
+# ======= NVM =============================
+
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && source "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# ======= PHP =============================
 
 # PHP ENV PATH
 export PATH="/usr/local/opt/php@7.2/bin:$PATH"
 
+# ======= ImageMagick =============================
+
 # Fix for imagemagic bundle compiler
 export PATH="/usr/local/opt/imagemagick@6/bin:$PATH"
-
-# Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
-export PATH="$PATH:$HOME/.rvm/bin"

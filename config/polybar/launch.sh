ebc4d2a36a781dba41cc39d6e549424b79e29aa0
Vlada <vladislav.trefil@protonmail.com>
2022-03-27 04:39:50 +0200

    polybar - add smaller screen support

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -8,10 +8,27 @@ sleep 1
 rm /tmp/polybar*
 rm /tmp/ipc-polybar
 
+largest_height=0
+largest_config=""
+largest_name=""
 
-polybar $CONFIG --reload example
+while IFS= read -r monitor; do  
+  height=$(echo $monitor | grep -oP "x\K([[:digit:]]{3,4})")
 
-# Launch polybar on all monitors
-# for m in $(polybar --list-monitors | cut -d":" -f1); do
-#   MONITOR=$m polybar $CONFIG --reload example &
-# done
+  if [[ $(echo "$height > 2000" | bc -l) -eq 1 ]]; then
+    config_name="large"
+  elif [[ $(echo "$height > 1000" | bc -l) -eq 1 ]]; then
+    config_name="medium"
+  else
+    config_name="medium"
+  fi
+
+  if [[ $(echo "$height > $largest_height" | bc -l) -eq 1 ]]; then
+    largest_height=$height
+    largest_name=$(echo $monitor | grep -oP "(.+):" | grep -oE ".*[^:]")
+    largest_config=$config_name
+  fi
+
+done < <(polybar --list-monitors)
+
+MONITOR=$largest_name polybar --reload $largest_config &

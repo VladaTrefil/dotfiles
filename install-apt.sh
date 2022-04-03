a65a5fbe80a0a99bebff0e51287c33695996d5a8
Vlada <vladislav.trefil@protonmail.com>
2022-04-03 22:31:24 +0200

    install - add custom apt script

---
+++ install-apt.sh
@@ -0,0 +1,83 @@
+#!/bin/zsh
+
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+APT_PACKAGES=(
+  "python3-dev"
+  "build-essential"
+  "nodejs"
+  "npm"
+  "ripgrep" # Command line search
+
+  # Desktop enviroment
+  "polybar"
+  "rofi"
+  "feh"
+  "picom"
+  "bc" # terminal calculation function
+
+  # Sinfin libs
+  "postgresql"
+  "postgresql-contrib"
+  "libpq-dev"
+  "imagemagick"
+  "libmagick++-dev"
+  "libmagickwand-dev"
+  "libmagickcore-dev"
+  "libimage-exiftool-perl"
+  "rubber"
+  "texlive-xetex"
+  "libsodium-dev"
+  "pdftk"
+  "ghostscript"
+  "redis-server"
+  "libvips42"
+  "gifsicle"
+  "libwebp-dev"
+  "webp"
+
+  # Apps
+  "konsole"
+  "syncthing"
+  "inkscape"
+
+  # NVIM
+  "pip" # pip install pynvim
+  "xclip" # terminal clipboard
+)
+
+if [ -f "`which apt`" ]; then
+  for p in $APT_PACKAGES; do
+    if [ $(dpkg-query -W -f='${Status}' $p 2>/dev/null | grep -c "ok installed") = "0" ]; then
+      echo "\n============================================================================="
+      echo -e "Installing ${GREEN}$p${NC}..."
+      sudo apt-get install $p -y --no-upgrade | grep -oP "^.*upgraded.*$"
+      echo -e "${GREEN}Installed $p${NC}"
+      echo "=============================================================================\n"
+    else
+      echo -e "${GREEN}$p${NC} already exists"
+    fi
+  done
+fi
+
+# Unofficial repos
+
+mkdir tmp_install
+
+# Install lens
+if [ ! -f "`which lens`" ]; then
+  echo 'Install Lens'
+  wget -O tmp_install/lens.deb https://lens-binaries.s3-eu-west-1.amazonaws.com/ide/Lens-5.3.4-latest.20220120.1.amd64.deb
+  sudo dpkg -i "tmp_install/lens.deb"
+fi
+
+sudo apt install -f
+rm -rf tmp_install
+
+if [ ! -f "`which brave-browser`" ]; then
+  sudo apt install apt-transport-https curl
+  curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+  echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+  sudo apt update
+fi
--- pre-install.sh
+++ pre-install.sh
@@ -8,7 +8,6 @@ fi
 
 # Check if NVIM is installed
 if [ -f "`which brew`" ]; then
-  echo 'Installing nvim'
   brew install -q neovim
   brew install -q lazygit
   brew install -q bat
@@ -47,25 +46,3 @@ if [ -z "`echo $SHELL | grep zsh`" ]; then
 else
   echo 'Already using zsh'
 fi
-
-mkdir tmp_install
-
-# Install lens
-if [ ! -f "`which lens`" ]; then
-  echo 'Install Lens'
-  wget -O tmp_install/lens.deb https://lens-binaries.s3-eu-west-1.amazonaws.com/ide/Lens-5.3.4-latest.20220120.1.amd64.deb
-  sudo dpkg -i "tmp_install/lens.deb"
-fi
-
-sudo apt install -f
-rm -rf tmp_install
-
-if [ ! -f "`which brave-browser`" ]; then
-  sudo apt install apt-transport-https curl
-  curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-  echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-  sudo apt update
-fi
-
-# source ~/.zshrc
-# source ~/.zprofile

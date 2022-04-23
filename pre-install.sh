2a74e9bf06f9b98ad15b5733c573a4084b16cd3e
Vlada <vladislav.trefil@protonmail.com>
2022-04-23 19:00:41 +0200

    install - tweak vim-plug installation

--- pre-install.sh
+++ pre-install.sh
@@ -49,8 +49,10 @@ if [ -f "`which brew`" ]; then
   fi
 fi
 
-if [ -f "`which nvim`" ]; then
-  sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+PLUG_DIR='"${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim'
+if [ -f "$PLUG_DIR" ]; then
+  echo 'Installing Vim-plug...'
+  sh -c 'curl -fLo $PLUG_DIR --create-dirs \
          https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 fi
 

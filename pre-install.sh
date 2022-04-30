c669f6d8ee0e2d7779a01c998553b2eb4fa2755a
Vlada <vladislav.trefil@protonmail.com>
2022-05-01 00:18:52 +0200

    install - tweaks

--- pre-install.sh
+++ pre-install.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-# if [ -f "`which apt`" ]; then
-#   sudo apt install zsh curl
-# fi
+if [ -f "`which apt`" ]; then
+  sudo apt install zsh curl
+fi
 
-# Check if Homebrew is installed
 if [ ! -f "`which brew`" ]; then
   echo 'Installing homebrew'
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-# Check if oh-my-zsh is installed
-if [ ! -d "$HOME/.oh-my-zsh" ]; then
+OMZDIR="$HOME/.oh-my-zsh"
+if [ ! -d "$OMZDIR" ]; then
   echo 'Installing oh-my-zsh'
   /bin/sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi

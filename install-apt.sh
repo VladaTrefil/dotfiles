a04aa7b1bdead45a50cd8d1d8d0dc23586a1c0b9
Vlada <vladislav.trefil@protonmail.com>
2022-04-30 23:49:13 +0200

    install - brave browser

--- install-apt.sh
+++ install-apt.sh
@@ -42,11 +42,19 @@ APT_PACKAGES=(
   "syncthing"
   "inkscape"
   "ffmpeg"
+  "brave-browser"
 
   # NVIM
   "pip" # pip install pynvim
   "xclip" # terminal clipboard
 )
+
+if [ ! -f "`which brave-browser`" ]; then
+  sudo apt install apt-transport-https curl
+  sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+  echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+  sudo apt update
+fi
 
 if [ -f "`which apt`" ]; then
   for p in $APT_PACKAGES; do
@@ -75,10 +83,3 @@ fi
 
 sudo apt install -f
 rm -rf tmp_install
-
-if [ ! -f "`which brave-browser`" ]; then
-  sudo apt install apt-transport-https curl
-  curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-  echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-  sudo apt update
-fi

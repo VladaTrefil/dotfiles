8ed64249fff41639c6dd10be6c110e1dac50aac7
Vlada <vladislav.trefil@protonmail.com>
2022-05-07 22:00:39 +0200

    aliases - cleanup

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -4,14 +4,14 @@ alias ...="cd ../.."
 alias ....="cd ../../.."
 alias .....="cd ../../../.."
 
-alias vim="nvim"
-
-alias ll="ls -l"
+alias l="ls -l"
 alias la="ls -la"
 
-alias cla="clear && ls -la"
-alias cll="clear && ls -l"
-alias cls="clear && ls"
+# Aliases to protect against overwriting
+alias cp='cp -i'
+alias mv='mv -i'
+
+alias cl="clear && ls -l"
 
 # Git aliases
 alias gaa="git add ."
@@ -29,57 +29,45 @@ alias gpo="git push origin"
 alias gpu="git pull"
 alias gpuo="git pull origin"
 
-alias rgc="r g folio:cell"
-alias rgm="r g migration"
-
 # Custom scripts
 alias clear-branches="sh ~/.local/bin/shell/clear-git-branches.sh"
 alias kill-server="sh ~/.local/bin/shell/kill-rails-server.sh"
 alias sinfin-init="sh ~/.local/bin/shell/init-sinfin-project.sh"
-
 alias folio-test-account="bundle exec rails runner \"eval(File.read '$HOME/.local/bin/folio/folio-test-account.rb')\""
 
 # Youtube-DL download music
 alias ydl="youtube-dl -x --audio-format \"mp3\" -o '%(title)s - %(uploader)s.%(ext)s' --embed-thumbnail"
 
-# Bat on debian
-# alias bat="batcat"
-
-# Aliases to protect against overwriting
-alias cp='cp -i'
-alias mv='mv -i'
-
-# colorize default commands
-# alias ls='ls --color=auto'
-# alias grep='grep --color=auto'
-# alias fgrep='fgrep --color=auto'
-# alias egrep='egrep --color=auto'
-# alias diff='diff --color=auto'
+# NVIM
+alias vim="nvim"
 
 # Docker
-# remove all images
-alias dockrai='docker system prune -a'
-# remove dangling images
-alias dockrdi='docker system prune'
+alias dockrai='docker system prune -a' # remove all images
+alias dockrdi='docker system prune' # remove dangling images
 
-#npm
+# Npm
 alias npmrs='npm run start'
 alias npmrd='npm run dev'
 alias npmrb='npm run build'
 
-#Ruby
+# Ruby
 alias r='bin/rails'
-alias rs='bin/rails s -p 3000'
+alias rs='r s -p 3000'
 alias rr='bin/rake'
+alias rt='PARALLEL_WORKERS=3 r test'
 
-alias rt='PARALLEL_WORKERS=3 bin/rails test'
+# Rails
+alias rgc="r g folio:cell"
+alias rgm="r g migration"
 
 # Python
 alias python='python3'
 alias python2='python3'
+
 # Exercism
 alias exes='exercism submit'
 
+# Etcher
 alias etcher='sudo ~/Images/balenaEtcher.AppImage'
 
 # Update dotfiles
@@ -96,7 +84,3 @@ alias cdgr='cd "$(git root)"'
 mkcd() {
   mkdir "${1}" && cd "${1}"
 }
-
-# LSD config
-# command -v lsd > /dev/null && alias ls='lsd --group-dirs first'
-# command -v lsd > /dev/null && alias ls='lsd --tree'

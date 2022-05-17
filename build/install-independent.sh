4747c324e3fe9bb056551dee78bc08ca80db2541
Vlada <vladislav.trefil@protonmail.com>
2022-05-17 20:59:40 +0200

    install - fix rvm

--- build/install-independent.sh
+++ build/install-independent.sh
@@ -45,10 +45,10 @@ if [ ! -f "`which rvm`" ]; then
   gpg --keyserver keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
   curl -sSL https://get.rvm.io | bash -s stable
 
-  if [[ -s "$XDG_CONFIG_HOME/.rvm/scripts/rvm" ]]; then
-    . "$XDG_CONFIG_HOME/.rvm/scripts/rvm"
+  if [[ -s "$HOME/.rvm/scripts/rvm" ]]; then
+    . "$HOME/.rvm/scripts/rvm"
 
-    export PATH="$PATH:$XDG_CONFIG_HOME/.rvm/bin"
+    export PATH="$PATH:$HOME/.rvm/bin"
 
     if [ -f "`which rvm`" ]; then
       rvm use 3.0.0 --default --install

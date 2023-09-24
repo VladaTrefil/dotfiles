e0714506d7e490fbe1c5f7df662e713e3ee6402e
Vlada <vladislav.trefil@protonmail.com>
2023-09-24 23:45:00 +0200

    cleanup

--- build/install-apt.sh
+++ build/install-apt.sh
@@ -122,6 +122,10 @@ APT_PACKAGES=(
   "binutils-arm-none-eabi"
   "libnewlib-arm-none-eabi"
 
+  # Brave browser
+  "apt-transport-https"
+  "curl"
+
   # Docker
   "ca-certificates"
   "curl"
@@ -134,8 +138,8 @@ APT_PACKAGES=(
   "docker-compose-plugin"
 )
 
-if [ ! -f "`which brave-browser`" ]; then
-  sudo apt install apt-transport-https curl
+if [ ! -f /etc/apt/sources.list.d/brave-browser-release.list ]; then
+  echo "Adding Brave browser repository..."
   sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
   echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 fi

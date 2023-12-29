d1a4a8c3652e0a440a367c5c2383e8f4abe74168
Vlada <vladislav.trefil@protonmail.com>
2023-12-29 22:32:22 +0100

    feat(install): replace rofi source build with apt

--- build/install-independent.sh
+++ build/install-independent.sh
@@ -213,59 +213,6 @@ fi
 # ────────────────────────────────────────────────────────────────────────────────────────────────────
 
 # ────────────────────────────────────────────────────────────────────────────────────────────────────
-# Rofi: {{{
-
-printf "\n────────────────────────────────────────────────────────────────────────────────────────────────────\n"
-echo "──  Rofi:"
-
-if [ ! -f "$(which rofi)" ]; then
-  cd ./tmp || exit
-
-  if [ ! -f "/usr/local/lib/libcheck.la" ]; then
-    git clone git@github.com:libcheck/check.git
-    git checkout 673dce1d61781c32b449bef0ee8711dc7e689170
-    git submodule update --init
-
-    mkdir ./check/build
-    cd ./check/build || exit
-
-    autoreconf --install
-    ../configure
-
-    make
-    sudo make install
-    sudo ldconfig
-  fi
-
-  git clone git@github.com:davatorium/rofi.git
-  git checkout 48ea818c699cd6d1424f4e98ec9236da4483f951
-  git submodule update --init
-
-  if [ -d "./rofi" ]; then
-    printf "\n─ installing rofi:\n"
-
-    mkdir ./rofi/build
-    cd ./rofi/build || exit
-
-    autoreconf -i
-    ../configure
-
-    make
-    sudo make install
-  else
-    echo "file not found"
-  fi
-
-  cd "$BASE_DIR" || exit
-else
-  echo "  installed"
-fi
-
-
-# }}}
-# ────────────────────────────────────────────────────────────────────────────────────────────────────
-
-# ────────────────────────────────────────────────────────────────────────────────────────────────────
 # QMK: {{{
 
 printf "\n────────────────────────────────────────────────────────────────────────────────────────────────────\n"

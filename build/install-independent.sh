5337503831ecb45efdccc156d1b0683db8f740ef
Vlada <vladislav.trefil@protonmail.com>
2023-12-29 16:46:41 +0100

    fix(install): small oversight

--- build/install-independent.sh
+++ build/install-independent.sh
@@ -293,10 +293,9 @@ fi
 # }}}
 # ────────────────────────────────────────────────────────────────────────────────────────────────────
 
-sh ./standalone/_git.sh
-
+# sh ./standalone/_git.sh
 #https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/
 # curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 # sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
-# rm -rf ./tmp
+rm -rf ./tmp
